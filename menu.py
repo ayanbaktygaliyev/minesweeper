c70429431b34061335c0541635e59fa99af66389
ayanbaktygaliyev <51299885+ayanbaktygaliyev@users.noreply.github.com>
2023-05-20 21:09:13 +0600

    main logic + bug fixes

--- menu.py
+++ menu.py
@@ -2,14 +2,22 @@ import pygame
 import pygame_menu
 import sys
 from game import Game
-
+# def values in case of empty loads on some of the fields
+x = 8
+y = 8
+probability = 0.15
 
 pygame.init()
 surface = pygame.display.set_mode((800, 600))
 
+
 def start_the_game():
+    mainmusic = pygame.mixer.Sound("game.ogg")
+    mainmusic.set_volume(0.1)
+    mainmusic.play()
+
     size = int(x), int(y)
-    prob = float(0.2)
+    prob = float(probability)
     g = Game(size, prob)
     g.run()
 
@@ -17,11 +25,11 @@ def start_the_game():
 def set_difficulty(value, difficulty):
     global probability
     if(difficulty==1):
-        probability = 0.1
+        probability = 0.15
     if(difficulty==2):
-        probability = 0.3
+        probability = 0.2
     if(difficulty==3):
-        probability = 0.5
+        probability = 0.3
 
 def boardsizex(value):
     global x
@@ -38,11 +46,10 @@ img.scale(0.2,0.2)
 menu = pygame_menu.Menu('Welcome to Minesweeper!', 800, 600,
                        theme=pygame_menu.themes.THEME_SOLARIZED)
 
-
 menu.add.image(image_path=img.copy())
 menu.add.text_input('Name :', default='Arman Suleimenov')
-menu.add.text_input('Board  Size (x) : ', maxchar= 2, onchange=boardsizex)
-menu.add.text_input('Board  Size (y) : ', maxchar= 2, onchange=boardsizey)
+menu.add.text_input('Board  Size (x) : ', maxchar= 2, default=8, onchange=boardsizex)
+menu.add.text_input('Board  Size (y) : ', maxchar= 2, default=8, onchange=boardsizey)
 menu.add.selector('Difficulty :', [('Easy', 1), ('Medium', 2), ('Hard', 3)], onchange=set_difficulty)
 menu.add.button('Play', start_the_game)
 menu.add.button('Quit', pygame_menu.events.EXIT)
